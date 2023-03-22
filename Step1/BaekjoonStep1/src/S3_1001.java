c3b1dc454dd720448c1944e89feb6d4a1ab3f640
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2023-03-22 13:25:36 +0900

    Step1_1001

--- Step1/BaekjoonStep1/src/S3_1001.java
+++ Step1/BaekjoonStep1/src/S3_1001.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
-public class S2_1000 {
-	public static void main(String[] arg) {
+public class S3_1001 {
+	
+	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int A = scanner.nextInt();
 		int B = scanner.nextInt();
 		
-		System.out.println(A+B);
+		System.out.println(A-B);
 		
-		scanner.close();	
+		scanner.close();
 	}
+
 }
