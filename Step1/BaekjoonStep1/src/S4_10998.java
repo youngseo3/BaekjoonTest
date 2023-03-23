210b92c2f0e300785e5d3ce6b903cb6bd8a60eaf
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2023-03-23 23:39:39 +0900

    Step1_10998

--- Step1/BaekjoonStep1/src/S4_10998.java
+++ Step1/BaekjoonStep1/src/S4_10998.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 
-public class S3_1001 {
+public class S4_10998 {
 	
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int A = scanner.nextInt();
 		int B = scanner.nextInt();
 		
-		System.out.println(A-B);
+		System.out.println(A*B);
 		
 		scanner.close();
+		
 	}
+
 }
