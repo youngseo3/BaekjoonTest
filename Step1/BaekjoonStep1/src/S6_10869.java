3cf30993b65582bc9630a508836606b7d7337324
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2023-03-25 16:53:21 +0900

    Step1_10869
    03.25

---
+++ Step1/BaekjoonStep1/src/S6_10869.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class S6_10869 {
+	public static void main (String args[]){
+		Scanner scanner = new Scanner (System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+		
+		scanner.close();
+	}
+}
