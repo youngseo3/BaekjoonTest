39fb4f618138c9c47a826982afceef0b73d67c3c
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2023-03-26 14:57:22 +0900

    Step1_10926
    03.26

---
+++ Step1/BaekjoonStep1/src/S7_10926.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class S7_10926 {
+	public static void main (String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		String id = scanner.nextLine();
+		
+		System.out.println(id+"??!");
+			
+	}
+}
