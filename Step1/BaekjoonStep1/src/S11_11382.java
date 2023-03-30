324ed38f058bf9363cabd861f3bb7c45aa91b46a
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2023-03-30 21:31:50 +0900

    Step1_11382
    03.30

---
+++ Step1/BaekjoonStep1/src/S11_11382.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class S11_11382 {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		
+		long A = scanner.nextLong();
+		long B = scanner.nextLong();
+		long C = scanner.nextLong();
+		
+		scanner.close();
+		
+		System.out.println(A+B+C);
+	}
+}
