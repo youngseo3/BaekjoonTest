236a5b7ba28744aee84ce1c91ee2c5da34f5296d
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2023-04-05 14:13:45 +0900

    Step2_14681
    04.05

---
+++ Step2/BaekjoonStep2/src/S24_14681.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class S24_14681 {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		
+		if (x > 0) {
+			if( y > 0)
+				System.out.println("1");
+			else {
+				System.out.println("4");
+			}
+		}
+		else {
+			if (y > 0) {
+				System.out.println("2");
+			}
+			else {
+				System.out.println("3");
+			}
+		scanner.close();
+		}
+	}
+}
