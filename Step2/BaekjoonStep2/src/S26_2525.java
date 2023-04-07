3b59b9b1753b2c7e977c92606acc7570d746deda
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2023-04-07 17:51:33 +0900

    Step2_2525
    04.07

---
+++ Step2/BaekjoonStep2/src/S26_2525.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class S26_2525 {
+	public static void main (String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		
+		int C = scanner.nextInt();
+		
+		scanner.close();
+		
+		int min = A * 60 + B;
+		min += C;
+		
+		int hour = (min/60)%24;
+		int minute = min % 60;
+		
+		System.out.println(hour + " " + minute);	
+	}
+}
