1238ca368418cdbf5f5f739dec82a62b7513b74b
SEUNG E <105618997+SeungEEE@users.noreply.github.com>
2023-04-09 19:10:11 +0900

    Step3_2739
    04.09

---
+++ Step3/BaekjoonStep3/src/S31_2739.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class S31_2739 {
+	public static void main (String args[]) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int N = scanner.nextInt();
+		
+		scanner.close();
+		
+		for(int i=1; i<10;i++){
+			System.out.println(N + " * " + i + " = " + (N*i));
+		}	
+	}
+}
