cb054d1af45af5b73a567aa5e19a8889cfc5e7b6
youngseo3 <wys4475@gmail.com>
2023-07-21 14:42:55 +0900

    S1_2557.java 수정

--- Step1/BaekjoonStep1/src/S1_2557.java
+++ Step1/BaekjoonStep1/src/S1_2557.java
@@ -1,6 +1,7 @@
 
 public class S1_2557 {
 	public static void main(String arg[]) {
-		System.out.print("Hello World!");
+		System.out.print("Hello World!\n");
+		System.out.println("youngseo3 commit");
 	}
 }
